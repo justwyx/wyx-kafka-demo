da06c7744be21ee12b594b53230f462b73f76986
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-10-27 16:18:01 +0800

    add:demo

---
+++ kafka-demo/src/main/java/com/wyx/kafkademo/producer/test1/OneProducerTest.java
@@ -0,0 +1,15 @@
+package com.wyx.kafkademo.producer.test1;
+
+import java.io.IOException;
+
+public class OneProducerTest {
+
+    public static void main(String[] args) throws IOException {
+        OneProducer producer = new OneProducer();
+        producer.sendMsg();
+        System.in.read();
+    }
+}
+
+
+
