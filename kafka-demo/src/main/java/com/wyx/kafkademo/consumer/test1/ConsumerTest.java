0a7bb939aad21b4afbb5ad258915cf242c10168d
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-10-27 18:15:09 +0800

    add

---
+++ kafka-demo/src/main/java/com/wyx/kafkademo/consumer/test1/ConsumerTest.java
@@ -0,0 +1,10 @@
+package com.wyx.kafkademo.consumer.test1;
+
+public class ConsumerTest {
+    public static void main(String[] args) {
+        SomeConsumer consumer = new SomeConsumer();
+        consumer.start();
+    }
+}
+
+
---
+++ kafka-demo/src/main/java/com/wyx/kafkademo/consumer/test3/AsyncManualTest.java
@@ -0,0 +1,10 @@
+package com.wyx.kafkademo.consumer.test3;
+
+public class AsyncManualTest {
+    public static void main(String[] args) {
+        AsyncManualConsumer consumer = new AsyncManualConsumer();
+        consumer.start();
+    }
+}
+
+
